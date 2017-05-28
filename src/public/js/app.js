762a9e23096badcf126e559a450715a27a61c9e6
Radhika <radshriv@in.ibm.com>
2017-05-28 16:04:23 +0530

    add app module

---
+++ src/public/js/app.js
@@ -0,0 +1,6 @@
+angular.module('stealie', ['ngRoute', 'ngMaterial']);
+
+// registering config block with the module
+angular.module('stealie')
+.config(['$routeProvider', function($routeProvider) {
+}])
