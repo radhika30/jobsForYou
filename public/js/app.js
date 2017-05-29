5307d8cfeaadaf8b15fe81baf53fcd0e1520aa08
Radhika <radshriv@in.ibm.com>
2017-05-29 21:35:56 +0530

    addition

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
-angular.module('jobsForYou', ['ngRoute', 'ngMaterial']);
+angular.module('jobsForYou', ['ngRoute', 'ngMaterial', 'toaster']);
 
 angular.module('jobsForYou')
 .config(['$routeProvider', function($routeProvider) {
+	console.log("module initializd")
 	// $routeProvider is used to register the route with the html and controller
 	$routeProvider.when('/dashboard', {
 		templateUrl: 'html/dashboard.html',
 		controller: 'dashboardController',
 		title: 'dashboard',
-		noLoginRequired: true,     
+		     
 	}).otherwise({
 		redirectTo: '/dashboard'
 	});
--- public/js/controller/dashboardController.js
+++ public/js/controller/dashboardController.js
@@ -1,6 +1,29 @@
-angular.module('jobsForYou').controller('dashboardCtrl', ['$scope', '$http', '$rootScope','$location', '$window','$document','$timeout','toaster',function($scope, $http, $rootScope, $sanitize, $location, $window,$document,$timeout,toaster) {
+angular.module('jobsForYou').controller('dashboardController', ['$scope', '$http', '$rootScope','$location', '$window','$document','$timeout','toaster',function($scope, $http, $rootScope, $location, $window, $document, $timeout, toaster) {
 	
 	
 	console.log("inside controller");
+	$scope.jobs=[{"name":"Accenture",
+	              "title":"Developer",
+				  "description":"Qualification-BTECH"},
+				  { "name":"Mphasis",
+	                "title":"Developer",
+				    "description":"Qualification-BTECH"}
+				  
+	             ];
+				 
+				 
+	var promiseObj = $http.post('/api/v1/jobsForYou');
+		promiseObj.then(function(res) {
+				if(res.data.status == 0){
+					$scope.edocs = res.data.results;
+				}
+				else if(res.data.status ==1 || res.data.status ==2){
+					toaster.pop({type: 'error', title: "Error", body:res.data.statusText});	
+				}
+			},
+			function(res) {  
+				toaster.pop({type: 'error', title: "Error", body:"Internal Server Error"});	
+			}
+		);
 	
 }]);
